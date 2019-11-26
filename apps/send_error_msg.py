d67fd8d37c561b046abbbb9325cb431fada292d5
zhulang <18720716380@163.com>
2019-11-26 10:43:06 +0800

    add

--- apps/send_error_msg.py
+++ apps/send_error_msg.py
@@ -18,7 +18,7 @@ cmd1 = "ls /root/nanodata_crawling/logs/error/ -l | grep error | tail -n 1 | awk
 res1 = os.popen(cmd1)
 res1_1=res1.read()
 
-cmd2="tail /root/nanodata_crawling/logs/%s" %res1_1
+cmd2="tail /root/nanodata_crawling/logs/%s/" %res1_1
 res2=os.popen(cmd2)
 res2_1=res2.read()
 
