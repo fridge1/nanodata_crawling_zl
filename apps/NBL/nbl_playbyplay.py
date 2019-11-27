7c8fa8d9c384d0df3fce20ec23ed6da5b5f8e3a1
zhulang <18720716380@163.com>
2019-11-27 14:16:01 +0800

    add

--- apps/NBL/nbl_playbyplay.py
+++ apps/NBL/nbl_playbyplay.py
@@ -14,16 +14,16 @@ class pbp_box(object):
     def __init__(self):
         self.team_id_get = get_team_id()
 
-    def pbp_box_live(self,data_queue,match_time):
+    def pbp_box_live(self,data_queue,match_id,match_time):
         while True:
             headers = {
                         'user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
                     }
-            url = 'https://www.fibalivestats.com/data/1307440/data.json'
+            url = 'https://www.fibalivestats.com/data/%s/data.json' % str(match_id)
             logger.info(url)
             pbp_res = requests.get(url,headers=headers)
             if int(match_time) >= int(time.time()) and pbp_res.status_code != 200:
-                logger.info('比赛未开赛.... %s' % str(1307440))
+                logger.info('比赛未开赛.... %s' % str(match_id))
                 time.sleep(10)
             else:
                 pbp_dict = json.loads(pbp_res.text)
@@ -222,7 +222,7 @@ class pbp_box(object):
                         word_text = pbp_info['actionType'] + ' ' + pbp_info['subType']
                     word_text_zh = translate(word_text)
                     data = {
-                        'id' : int(1307440),
+                        'id' : int(match_id),
                         'text':word_text_zh,
                         'period':int(period),
                         'type':int(type),
@@ -240,22 +240,22 @@ class pbp_box(object):
                         'location_y' : int(location_y),
                     }
                     playbyplay_list.append(data)
-                match_data_boxscore = {'match': {'id': int(1307440),
+                match_data_boxscore = {'match': {'id': int(match_id),
                                                  'basketball_items': {
                                                      'player_stat': {
                                                          'items': player_stats_list},
                                                      'team_stat': {'items': team_stats_list}
                                                  }}}
                 data_queue.put(match_data_boxscore)
-                logger.info('球员技术统计推送完成... %s' % str(1307440))
-                match_data_playbyplay = {'match': {'id': int(1307440),
+                logger.info('球员技术统计推送完成... %s' % str(match_id))
+                match_data_playbyplay = {'match': {'id': int(match_id),
                                                    'basketball_items': {
                                                        'incident': {
                                                            'period': period_total,
                                                            'items': playbyplay_list}
                                                        }}}
                 data_queue.put(match_data_playbyplay)
-                logger.info('球员技术文字直播推送完成... %s' % str(1307440))
+                logger.info('球员技术文字直播推送完成... %s' % str(match_id))
                 if playbyplay_list[-1]['text'] == '比赛结束':
                     break
                 else:
@@ -274,10 +274,9 @@ class pbp_box(object):
             res = requests.get(url,headers=headers)
             match_dict = json.loads(res.text)
             for id_time in match_dict['data']:
-                # match_id = id_time['matchId']
+                match_id = id_time['matchId']
                 match_time = change_bjtime(id_time['matchTimeUTC'])
-                # threading.Thread(target=pbp_box().pbp_box_live,args=(data_queue,match_id,match_time)).start()
-                pbp_box().pbp_box_live(data_queue,match_time)
+                threading.Thread(target=pbp_box().pbp_box_live,args=(data_queue,match_id,match_time)).start()
         except:
             dingding_alter(traceback.format_exc())
             logger.error(traceback.format_exc())
