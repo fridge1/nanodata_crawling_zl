0a6e272d91091cbe892dc0f40708f9e9a86e8950
zhulang <18720716380@163.com>
2019-12-12 19:29:05 +0800

    add

--- apps/send_error_msg.py
+++ apps/send_error_msg.py
@@ -3,7 +3,6 @@ import requests
 import json
 
 
-
 def dingding_alter(text):  # 程序报警
     url = 'https://oapi.dingtalk.com/robot/send?access_token=adef51bdf91a51103717ba9178986d69eaeff094998671b0bfb819f11d9b9c3d'
     data = {
