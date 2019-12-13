4e5a10b8828518b6dc05ea5307cb6c10b32db62f
zhulang <18720716380@163.com>
2019-12-13 18:30:33 +0800

    add

--- apps/NBL/nbl_score_svr.py
+++ apps/NBL/nbl_score_svr.py
@@ -7,8 +7,7 @@ from common.libs.log import LogMgr
 from common.libs.pbjson import dict2pb
 from common.utils import NatsSvr
 from pb.nana.biz.base_pb2 import Request, Result
-from pb.nana.biz.example import demo_pb2
-from pb.nana.biz.japan_ball import match_pb2
+from pb.nana.biz.sport import match_pb2
 from apps.NBL.nbl_score import GetScores
 from apps.NBL.tools import get_match_id_score
 
@@ -32,13 +31,11 @@ class NblBasketballScore(object):
     async def start(self, topic):
         self.topic = topic
         self.nc = await NatsSvr.get_stan('hub.nats')
-        await self.start_feed_rpc()
         await self.start_feed()
 
     async def start_feed(self):
         match_id_list = get_match_id_score()
         while True:
-            time.sleep(2)
             coro = [asyncio.create_task(GetScores().get_scores(game_id)) for game_id in match_id_list]
             data = await asyncio.gather(*coro)
             for i in data:
@@ -49,51 +46,10 @@ class NblBasketballScore(object):
                 else:
                     logger.info('比赛未开赛。。。')
 
-    async def start_feed_rpc(self):
-        rpc_topic = '%s.rpc' % self.topic
-        await self.nc.subscribe(rpc_topic, cb=self.msg_handler, queue='rpc')
-
-    # 消息处理
-    async def msg_handler(self, msg):
-        try:
-            req_pb = Request()
-            req_pb.ParseFromString(msg.data)
-            print(msg.subject)
-            if req_pb.code == demo_pb2.SID_DEMO_TIME_REQ:
-                await self.pub_time_data(msg.reply, '')
-            elif req_pb.code == demo_pb2.SID_DEMO_HEARTBEAT_REQ:
-                await self.pub_check_data(msg.reply)
-            elif req_pb.code == demo_pb2.SID_DEMO_RESTART_REQ:
-                await self.pub_restart_data(msg.reply)
-                await asyncio.sleep(1)
-        except Exception:
-            # 将异常信息记录到log日志中
-            logger.error(traceback.format_exc())
 
     async def pub_time_data(self, topic, match_data):
-        self.cnt += 1
-        # print('aaaa')
-        data_pb = dict2pb(match_pb2.FibaMatchRes, match_data)
-        res = {
-            'code': match_pb2.SID_FIBAMATCH_RES,
-            'data': data_pb.SerializeToString()
-        }
-        res_bin = dict2pb(Result, res).SerializeToString()
-        await self.nc.publish(topic, res_bin)
-        logger.info('推送成功')
+        data_pb = dict2pb(match_pb2.SportsMatchesRes, match_data).SerializeToString()
+        print(data_pb)
+        await self.nc.publish(topic, data_pb)
 
-    # 发布检查响应
-    async def pub_check_data(self, topic):
-        result = {
-            'code': demo_pb2.SID_DEMO_HEARTBEAT_RES,
-        }
-        res_bin = dict2pb(Result, result).SerializeToString()
-        await self.nc.publish(topic, res_bin)
 
-    # 发布重启响应
-    async def pub_restart_data(self, topic):
-        result = {
-            'code': demo_pb2.SID_DEMO_RESTART_RES,
-        }
-        res_bin = dict2pb(Result, result).SerializeToString()
-        await self.nc.publish(topic, res_bin)
