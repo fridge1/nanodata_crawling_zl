b3278a981d156e930ec8e254485dcfb087252b92
zhulang <18720716380@163.com>
2019-12-17 20:06:50 +0800

    add

--- actions/kbl_basketball.py
+++ actions/kbl_basketball.py
@@ -5,11 +5,13 @@ from apps.kbl.kbl_match import GetMatchObj
 from apps.kbl.kbl_play_team_stat import PlayerTeamStats
 from apps.kbl.kbl_score_svr import KblBasketballScore
 
+# from apps.kbl.kbl_score_client import BeitaiCbaScroeFeedSvr
 
 
 servers = ["nats://hub.nats.namincs.com:4222"]
 user = 'nana'
 password = 'aa123456'
+
 
 def start_kbl_basketball_svr(opts):
     topic = 'kbl.bk.live'
@@ -20,6 +22,7 @@ def start_kbl_basketball_client(opts):
     topic = 'kbl.bk.live'
     asyncio.run(KblBasketballFeedClient().start(topic=topic))
 
+
 def start_kbl_basketball_score_svr(opts):
     topic = 'bk.score.live'
     asyncio.run(KblBasketballScore().start(topic=topic))
@@ -28,17 +31,16 @@ def start_kbl_basketball_score_svr(opts):
 def start_kbl_basketball_match(opts):
     asyncio.run(GetMatchObj().run())
 
+
 def kbl_basketball_player_team_stat(opts):
     PTS = PlayerTeamStats()
     asyncio.run(PTS.run())
 
 
-
-
 kbl_basketball_actions = {
-    'start_kbl_basketball_match' : start_kbl_basketball_match,
-    'start_kbl_basketball_svr' : start_kbl_basketball_svr,
-    'start_kbl_basketball_client' : start_kbl_basketball_client,
-    'kbl_basketball_player_team_stat' : kbl_basketball_player_team_stat,
-    'start_kbl_basketball_score_svr' : start_kbl_basketball_score_svr,
+    'start_kbl_basketball_match': start_kbl_basketball_match,
+    'start_kbl_basketball_svr': start_kbl_basketball_svr,
+    'start_kbl_basketball_client': start_kbl_basketball_client,
+    'kbl_basketball_player_team_stat': kbl_basketball_player_team_stat,
+    'start_kbl_basketball_score_svr': start_kbl_basketball_score_svr,
 }
--- apps/kbl/kbl_score_svr.py
+++ apps/kbl/kbl_score_svr.py
@@ -9,6 +9,7 @@ from pb.nana.biz.sport import match_pb2
 from apps.kbl.kbl_score import GetScores
 from apps.kbl.tools import get_match_id_start
 
+
 def now():
     return int(time.time() * 1000)
 
@@ -34,7 +35,8 @@ class KblBasketballScore(object):
     async def start_feed(self):
         match_id_dict = get_match_id_start()
         while True:
-            coro = [asyncio.create_task(GetScores().get_scores(game_id,match_id)) for game_id,match_id in match_id_dict.items()]
+            coro = [asyncio.create_task(GetScores().get_scores(game_id, match_id)) for game_id, match_id in
+                    match_id_dict.items()]
             data = await asyncio.gather(*coro)
             for i in data:
                 if i != 0:
@@ -44,9 +46,7 @@ class KblBasketballScore(object):
                 else:
                     logger.info('比赛未开赛。。。')
 
-
     async def pub_time_data(self, topic, match_data):
         data_pb = dict2pb(match_pb2.SportsMatchesRes, match_data).SerializeToString()
+        print(data_pb)
         await self.nc.publish(topic, data_pb)
-
-
