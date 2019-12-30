ff6c62de8eaa9ee5ab805442e0a7882d97d0b499
zhulang <18720716380@163.com>
2019-12-30 09:55:36 +0800

    add

--- actions/acb_basketball.py
+++ actions/acb_basketball.py
@@ -4,7 +4,6 @@ from apps.ACB.acb_svr import AcbBasketballFeedSvr
 from apps.ACB.abc_match import GetMatchInfo
 
 
-
 def start_acb_basketball_svr(opts):
     topic = 'acb.bk.live'
     asyncio.run(AcbBasketballFeedSvr().start(topic))
@@ -14,16 +13,13 @@ def start_acb_basketball_client(opts):
     topic = 'acb.bk.live'
     asyncio.run(AcbBasketballFeedClient().start(topic))
 
+
 def acb_basketball_match(opts):
     GetMatchInfo().get_match_info()
-
-
-
 
 
 acb_basketball_actions = {
     'start_acb_basketball_svr': start_acb_basketball_svr,
     'start_acb_basketball_client': start_acb_basketball_client,
+    'acb_basketball_match': acb_basketball_match,
 }
-
-
