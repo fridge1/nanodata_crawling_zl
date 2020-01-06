ad7649dac317214c0bd8aa8bc8d58cb983af7c49
zhulang <18720716380@163.com>
2020-01-06 15:03:08 +0800

    add

--- apps/tennis_WTA/wta_competition.py
+++ apps/tennis_WTA/wta_competition.py
@@ -46,8 +46,6 @@ class GetCompetitionInfo(object):
             competition_info['prizeMoneyCurrency'] = 1
             competition_info['inOutdoor'] = self.door_dict[info['inOutdoor']]
             city_name = info['city']
-            print(city_name)
-            print(type(city_name))
             country_name = info['country']
             if city_name in list(self.city_id.keys()):
                 competition_info['city_id'] = self.city_id[city_name]
@@ -68,8 +66,8 @@ class GetCompetitionInfo(object):
 
 
     def run(self):
-        year = '2019'
-        self.competition_info(year)
+        for year in range(2010,2020):
+            self.competition_info(str(year))
 
 
 
