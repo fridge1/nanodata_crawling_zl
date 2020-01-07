2cb7a44a0e0a303499e2f5f0b3d689b84387bc85
zhulang <18720716380@163.com>
2020-01-07 18:01:17 +0800

    add

--- apps/tennis_WTA/wta_single_player_stat.py
+++ apps/tennis_WTA/wta_single_player_stat.py
@@ -23,37 +23,37 @@ class GetSinglePlayerStat(object):
         res = requests.get(url,headers=self.headers)
         if res.text:
             stat_info = json.loads(res.text)
-            player_stat['key'] = str(year)+str(player_id)
-            player_stat['season_id'] = year
-            player_stat['sport_id'] = 3
-            player_stat['player_id'] = player_id
-            if 'stats' not in stat_info.keys():
+            if 'stats' in list(stat_info.keys()):
+                player_stat['key'] = str(year)+str(player_id)
+                player_stat['season_id'] = year
+                player_stat['sport_id'] = 3
+                player_stat['player_id'] = player_id
+                player_stat['aces'] = stat_info['stats']['Aces']
+                player_stat['double_faults'] = stat_info['stats']['Double_Faults']
+                player_stat['service_points_win'] = stat_info['stats']['service_points_won_percent']
+                player_stat['first_serve'] = stat_info['stats']['first_serve_percent']
+                player_stat['first_serve_win'] = stat_info['stats']['first_serve_won_percent']
+                player_stat['second_serve_win'] = stat_info['stats']['second_serve_won_percent']
+                player_stat['break_point_saved'] = stat_info['stats']['breakpoint_saved_percent']
+                player_stat['service_games_win'] = stat_info['stats']['service_games_won_percent']
+                player_stat['service_games_played'] = stat_info['stats']['Service_Games_Played']
+                player_stat['return_points_win'] = stat_info['stats']['return_points_won_percent']
+                player_stat['first_return_points_won'] = stat_info['stats']['first_return_percent']
+                player_stat['second_return_points_won'] = stat_info['stats']['second_return_percent']
+                player_stat['break_point_converted'] = stat_info['stats']['breakpoint_converted_percent']
+                player_stat['break_points_lost'] = stat_info['stats']['Break_Points_Lost']
+                player_stat['return_games_win'] = stat_info['stats']['return_games_won_percent']
+                player_stat['return_games_played'] = stat_info['stats']['Return_Games_Played']
+                player_stat['break_points_faced'] = stat_info['stats']['Break_Points_Faced']
+                player_stat['match_count'] = stat_info['stats']['MatchCount']
+                TennisSinglePlayerStatBySeason.upsert(
+                    self.session,
+                    'key',
+                    player_stat
+                )
+                print(player_stat)
+            else:
                 print(stat_info)
-                exit()
-            player_stat['aces'] = stat_info['stats']['Aces']
-            player_stat['double_faults'] = stat_info['stats']['Double_Faults']
-            player_stat['service_points_win'] = stat_info['stats']['service_points_won_percent']
-            player_stat['first_serve'] = stat_info['stats']['first_serve_percent']
-            player_stat['first_serve_win'] = stat_info['stats']['first_serve_won_percent']
-            player_stat['second_serve_win'] = stat_info['stats']['second_serve_won_percent']
-            player_stat['break_point_saved'] = stat_info['stats']['breakpoint_saved_percent']
-            player_stat['service_games_win'] = stat_info['stats']['service_games_won_percent']
-            player_stat['service_games_played'] = stat_info['stats']['Service_Games_Played']
-            player_stat['return_points_win'] = stat_info['stats']['return_points_won_percent']
-            player_stat['first_return_points_won'] = stat_info['stats']['first_return_percent']
-            player_stat['second_return_points_won'] = stat_info['stats']['second_return_percent']
-            player_stat['break_point_converted'] = stat_info['stats']['breakpoint_converted_percent']
-            player_stat['break_points_lost'] = stat_info['stats']['Break_Points_Lost']
-            player_stat['return_games_win'] = stat_info['stats']['return_games_won_percent']
-            player_stat['return_games_played'] = stat_info['stats']['Return_Games_Played']
-            player_stat['break_points_faced'] = stat_info['stats']['Break_Points_Faced']
-            player_stat['match_count'] = stat_info['stats']['MatchCount']
-            TennisSinglePlayerStatBySeason.upsert(
-                self.session,
-                'key',
-                player_stat
-            )
-            # print(player_stat)
         else:
             print('该球员没有该赛季的技术统计。。。')
 
