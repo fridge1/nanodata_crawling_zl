5a4afc6b99b8d78a404bc820962b620e76c04ca4
zhulang <18720716380@163.com>
2020-01-08 11:51:13 +0800

    add

--- apps/eur_basketball_spider/eur_table.py
+++ apps/eur_basketball_spider/eur_table.py
@@ -1,5 +1,7 @@
 import requests
 from apps.eur_basketball_spider.tools import tree_parse
+import re
+
 
 
 class GetTable(object):
@@ -8,20 +10,11 @@ class GetTable(object):
             'user_agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
         }
 
-
-
-    def table_info(self):
-        url = 'https://www.euroleague.net/main/standings'
-        res = requests.get(url,headers=self.headers)
-        res_tree = tree_parse(res)
-        season_urls = res_tree.xpath('//div[@class="styled-select"][1]/select/option/@value')
-        return season_urls
-
     def season_type_url(self):
-        season_urls = self.table_info()
         total_type_urls = []
-        for season_url in season_urls:
-            res = requests.get('https://www.euroleague.net' + season_url,headers=self.headers)
+        for season_id in range(2016, 2020):
+            res = requests.get('https://www.euroleague.net/main/standings?seasoncode=%s' % season_id,
+                               headers=self.headers)
             res_tree = tree_parse(res)
             type_urls = res_tree.xpath('//div[@class="styled-select"][2]/select/option/@value')
             total_type_urls += type_urls
@@ -40,11 +33,16 @@ class GetTable(object):
     def match_url(self):
         total_round_urls = self.round_url()
         for round_url in total_round_urls:
-            res = requests.get('https://www.euroleague.net'+round_url,headers=self.headers)
+            res = requests.get('https://www.euroleague.net' + round_url, headers=self.headers)
             res_tree = tree_parse(res)
             match_urls = res_tree.xpath('//ul[@class="rounds-list"]/li/a/@href')
-            print(match_urls)
-
-
-
-
+            for match_url in match_urls:
+                res = requests.get('https://www.euroleague.net'+match_url,headers=self.headers)
+                res_tree = tree_parse(res)
+                typecode = re.findall(r'phasetypecode=(.*?)\+',match_url)[0]
+                season_id = re.findall(r'seasoncode=E(.*?)',match_url)[0]
+                if 'RS' or 'TS' in typecode:
+                    stage_name_zh = '欧篮联%s-%s赛季常规赛' % (str(season_id), str(season_id + 1))
+                else:
+                    stage_name_zh = '欧篮联%s-%s赛季季后赛' % (str(season_id), str(season_id + 1))
+                print(typecode,season_id)
