1a7c44a2e09b9fea190a33be8f82ac98179ba07f
zhulang <18720716380@163.com>
2020-01-09 19:55:19 +0800

    add

--- apps/tennis_WTA/wta_single_rank.py
+++ apps/tennis_WTA/wta_single_rank.py
@@ -3,6 +3,7 @@ from orm_connection.orm_session import MysqlSvr
 from orm_connection.tennis import TennisPlayerInfoSingleRank
 from apps.tennis_WTA.tools import rank_match_bjtime,get_proxy
 import json
+import traceback
 from apps.tennis_WTA.get_monday_date import GetMondayDate
 from common.libs.log import LogMgr
 
@@ -29,31 +30,34 @@ class GetSingleRankInfo(object):
         if response.text == '':
             logger.info('没有排名数据。。。')
         else:
-            rank_info = json.loads(response.text)
-            for info in rank_info:
-                player_info = {}
-                player_info['player_id'] = info['player']['id']
-                player_info['key'] = str(date) + str(player_info['player_id'])
-                player_info['sport_id'] = 3
-                player_info['name_en'] = info['player']['fullName']
-                player_info['ranking'] = info['ranking']
-                player_info['points'] = info['points']
-                player_info['scope_date'] = rank_match_bjtime(date)
-                player_info['stat_cycle'] = 7
-                player_info['promotion'] = info['movement']
-                player_info['season_id'] = 2019
-                if player_info['promotion'] > 0:
-                    player_info['promotion_type'] = 1
-                elif player_info['promotion'] == 0:
-                    player_info['promotion_type'] = 0
-                else:
-                    player_info['promotion_type'] = 2
-                TennisPlayerInfoSingleRank.upsert(
-                    self.session,
-                    'key',
-                    player_info
-                )
-                logger.info(player_info)
+            try:
+                rank_info = json.loads(response.text)
+                for info in rank_info:
+                    player_info = {}
+                    player_info['player_id'] = info['player']['id']
+                    player_info['key'] = str(date) + str(player_info['player_id'])
+                    player_info['sport_id'] = 3
+                    player_info['name_en'] = info['player']['fullName']
+                    player_info['ranking'] = info['ranking']
+                    player_info['points'] = info['points']
+                    player_info['scope_date'] = rank_match_bjtime(date)
+                    player_info['stat_cycle'] = 7
+                    player_info['promotion'] = info['movement']
+                    player_info['season_id'] = 2019
+                    if player_info['promotion'] > 0:
+                        player_info['promotion_type'] = 1
+                    elif player_info['promotion'] == 0:
+                        player_info['promotion_type'] = 0
+                    else:
+                        player_info['promotion_type'] = 2
+                    TennisPlayerInfoSingleRank.upsert(
+                        self.session,
+                        'key',
+                        player_info
+                    )
+                    logger.info(player_info)
+            except:
+                logger.error(traceback.format_exc())
 
     def run(self):
         monday_date_list = GetMondayDate().run(2020)
