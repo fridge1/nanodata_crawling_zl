5229496c8cf16d869b74b3c203fede5734b1256d
zhulang <18720716380@163.com>
2020-01-14 16:38:38 +0800

    add

--- apps/tennis_WTA/wta_player_match_stat.py
+++ apps/tennis_WTA/wta_player_match_stat.py
@@ -18,7 +18,10 @@ class GetMatchPlayerStat(object):
             competition_id = row.key[4:-5]
             match_id = row.key[-5:]
             player_ids = [row.home_player_id] + [row.away_player_id]
-            self.get_player_stat(competition_id, season_id, match_id, row.id, player_ids)
+            if season_id != 2017:
+                self.get_player_stat(competition_id, season_id, match_id, row.id, player_ids)
+            else:
+                self.no_match_stat(competition_id, season_id, match_id, row.id, player_ids)
 
     def get_player_stat(self, competition_id, season_id, match_id, id, player_ids):
         url = 'https://api.wtatennis.com/tennis/tournaments/%s/%s/matches/%s/stats' % (
@@ -110,8 +113,11 @@ class GetMatchPlayerStat(object):
                         match_player_stat['first_serve'] = (info['ptsplayed1stserva'] / info['totservplayeda']) * 100
                         match_player_stat['first_serve_win'] = (info['ptswon1stserva'] / info[
                             'ptsplayed1stserva']) * 100
-                        match_player_stat['second_serve_win'] = (info['ptstotwonserva'] - info['ptswon1stserva']) / (
-                                info['totservplayeda'] - info['ptsplayed1stserva']) * 100
+                        if info['totservplayeda'] != info['ptsplayed1stserva']:
+                            match_player_stat['second_serve_win'] = (info['ptstotwonserva'] - info['ptswon1stserva']) / (
+                                    info['totservplayeda'] - info['ptsplayed1stserva']) * 100
+                        else:
+                            match_player_stat['second_serve_win'] = 0
                         if info['breakptsplayedb'] != 0:
                             match_player_stat['break_point_saved'] = info['breakptsconvb'] / info[
                                 'breakptsplayedb'] * 100
@@ -144,8 +150,11 @@ class GetMatchPlayerStat(object):
                         match_player_stat['first_serve'] = (info['ptsplayed1stservb'] / info['totservplayedb']) * 100
                         match_player_stat['first_serve_win'] = (info['ptswon1stservb'] / info[
                             'ptsplayed1stservb']) * 100
-                        match_player_stat['second_serve_win'] = (info['ptstotwonservb'] - info['ptswon1stservb']) / (
-                                info['totservplayedb'] - info['ptsplayed1stservb']) * 100
+                        if info['totservplayedb'] != info['ptsplayed1stservb']:
+                            match_player_stat['second_serve_win'] = (info['ptstotwonservb'] - info['ptswon1stservb']) / (
+                                    info['totservplayedb'] - info['ptsplayed1stservb']) * 100
+                        else:
+                            match_player_stat['second_serve_win'] = 0
                         if info['breakptsplayeda'] != 0:
                             match_player_stat['break_point_saved'] = info['breakptsconva'] / info[
                                 'breakptsplayeda'] * 100
@@ -173,5 +182,95 @@ class GetMatchPlayerStat(object):
                     print('player_stat:',match_player_stat)
 
 
-if __name__ == '__main__':
-    GetMatchPlayerStat().get_match_info()
+    def no_match_stat(self,competition_id, season_id, match_id, id, player_ids):
+        url = 'https://api.wtatennis.com/tennis/tournaments/%s/%s/matches/%s/stats' % (
+            competition_id, season_id, match_id)
+        print(url)
+        res = requests.get(url, headers=self.headers)
+        infos = json.loads(res.text)
+        if infos:
+            for index in range(len(player_ids)):
+                for info in infos:
+                    match_player_stat = {}
+                    if index == 0:
+                        match_player_stat['set_num'] = info['setnum']
+                        match_player_stat['key'] = str(id) + str(player_ids[index]) + str(info['setnum'])
+                        match_player_stat['player_ids'] = player_ids[index]
+                        match_player_stat['sport_id'] = 3
+                        match_player_stat['match_id'] = id
+                        match_player_stat['aces'] = info['acesa']
+                        match_player_stat['double_faults'] = info['dblflta']
+                        match_player_stat['service_points_win'] = (info['ptstotwonserva'] / info[
+                            'totservplayeda']) * 100
+                        match_player_stat['first_serve'] = (info['ptsplayed1stserva'] / info['totservplayeda']) * 100
+                        match_player_stat['first_serve_win'] = (info['ptswon1stserva'] / info[
+                            'ptsplayed1stserva']) * 100
+                        if info['totservplayeda'] != info['ptsplayed1stserva']:
+                            match_player_stat['second_serve_win'] = (info['ptstotwonserva'] - info['ptswon1stserva']) / (
+                                    info['totservplayeda'] - info['ptsplayed1stserva']) * 100
+                        else:
+                            match_player_stat['second_serve_win'] = 0
+                        if info['breakptsplayedb'] != 0:
+                            match_player_stat['break_point_saved'] = info['breakptsconvb'] / info[
+                                'breakptsplayedb'] * 100
+                        else:
+                            match_player_stat['break_point_saved'] = 0
+                        match_player_stat['service_games_win'] = info['ptstotwonserva'] / info['totservplayeda'] * 100
+                        match_player_stat['service_games_played'] = info['servgamesplayeda']
+                        match_player_stat['return_games_played'] = info['servgamesplayedb']
+                        match_player_stat['first_return_points_won'] = info['pts1stservlostb'] / info[
+                            'ptsplayed1stservb'] * 100
+                        match_player_stat['second_return_points_won'] = ((info['totservplayedb'] - info[
+                            'ptsplayed1stservb']) - (info['ptstotwonservb'] - info['ptswon1stservb'])) / (
+                                                                                info['totservplayedb'] - info[
+                                                                            'ptsplayed1stservb']) * 100
+                        if info['breakptsplayeda'] != 0:
+                            match_player_stat['break_point_converted'] = info['breakptsconva'] / info[
+                                'breakptsplayeda'] * 100
+                        else:
+                            match_player_stat['break_point_converted'] = 0
+                    else:
+                        match_player_stat['set_num'] = info['setnum']
+                        match_player_stat['key'] = str(id) + str(player_ids[index]) + str(info['setnum'])
+                        match_player_stat['player_ids'] = player_ids[index]
+                        match_player_stat['sport_id'] = 3
+                        match_player_stat['match_id'] = id
+                        match_player_stat['aces'] = info['acesb']
+                        match_player_stat['double_faults'] = info['dblfltb']
+                        match_player_stat['service_points_win'] = (info['ptstotwonservb'] / info[
+                            'totservplayedb']) * 100
+                        match_player_stat['first_serve'] = (info['ptsplayed1stservb'] / info['totservplayedb']) * 100
+                        match_player_stat['first_serve_win'] = (info['ptswon1stservb'] / info[
+                            'ptsplayed1stservb']) * 100
+                        if info['totservplayedb'] != info['ptsplayed1stservb']:
+                            match_player_stat['second_serve_win'] = (info['ptstotwonservb'] - info['ptswon1stservb']) / (
+                                    info['totservplayedb'] - info['ptsplayed1stservb']) * 100
+                        else:
+                            match_player_stat['second_serve_win'] = 0
+                        if info['breakptsplayeda'] != 0:
+                            match_player_stat['break_point_saved'] = info['breakptsconva'] / info[
+                                'breakptsplayeda'] * 100
+                        else:
+                            match_player_stat['break_point_saved'] = 0
+                        match_player_stat['service_games_win'] = info['ptstotwonservb'] / info['totservplayedb'] * 100
+                        match_player_stat['service_games_played'] = info['servgamesplayedb']
+                        match_player_stat['return_games_played'] = info['servgamesplayeda']
+                        match_player_stat['first_return_points_won'] = info['pts1stservlosta'] / info[
+                            'ptsplayed1stserva'] * 100
+                        match_player_stat['second_return_points_won'] = ((info['totservplayedb'] - info[
+                            'ptsplayed1stservb']) - (info['ptstotwonservb'] - info['ptswon1stservb'])) / (
+                                                                                info['totservplayedb'] - info[
+                                                                            'ptsplayed1stservb']) * 100
+                        if info['breakptsplayedb'] != 0:
+                            match_player_stat['break_point_converted'] = info['breakptsconvb'] / info[
+                                'breakptsplayedb'] * 100
+                        else:
+                            match_player_stat['break_point_converted'] = 0
+                    TennisMatchPlayerStat.upsert(
+                        self.session,
+                        'key',
+                        match_player_stat
+                    )
+                    print('2017player_stat:', match_player_stat)
+
+
